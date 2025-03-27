86ae5527aa23dc2964dead73924f4d2a81739fc8
Tim Trojner <tim.trojner@gmail.com>
2025-03-27 16:02:12 +0100

    implemented JOSS ✨

--- src/players/random_player.py
+++ src/players/random_player.py
@@ -1,7 +1,6 @@
 import numpy as np
 from players.base_player import BasePlayer
 
-
 class Random(BasePlayer):
     def initial_action(self):
         return np.random.choice(["cooperate", "defect"])
@@ -65,6 +64,35 @@ class Tester(BasePlayer):
             else:
                 # If opponent forgave our defection, exploit them by defecting
                 action = "defect"
+        
+        self.past_actions.append(action)
+        self.num_games += 1
+        return action
+
+class JOSS(BasePlayer):
+    def __init__(self, defection_probability=0.1):
+        super().__init__()
+        self.defection_probability = defection_probability
+        
+    def initial_action(self):
+        # Start by cooperating (like Tit-for-Tat)
+        return "cooperate"
+    
+    def get_action(self, opponents_history: list):
+        if len(opponents_history) == 0:
+            action = self.initial_action()
+        else:
+            # If opponent defected, always defect
+            if opponents_history[-1] == "defect":
+                action = "defect"
+            else:
+                # Opponent cooperated
+                # With probability defection_probability, defect anyway
+                if np.random.random() < self.defection_probability:
+                    action = "defect"
+                else:
+                    # Otherwise cooperate (follow Tit-for-Tat)
+                    action = "cooperate"
         
         self.past_actions.append(action)
         self.num_games += 1
